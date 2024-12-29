10b4ad0b11f09964f84b2fd92d9eb63dbb19fd9d
Ihor Chovpan <i.chovpan@gmail.com>
2024-12-29 13:06:05 -0500

    Make a primitive menu

---
+++ frontend/components/RmFiles.svelte
@@ -0,0 +1,3 @@
+<div>
+    <h1>Welcome to RmFiles</h1>
+</div>
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,4 +1,5 @@
-import './style.css'
+import './assets/css/materialize.min.css'
+import './assets/css/icons.css'
 import App from './App.svelte'
 
 const app = new App({
