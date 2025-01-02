b0eb1b6fb537d5fc993ce9bb683f994e66bb6739
Ihor Chovpan <i.chovpan@gmail.com>
2025-01-01 19:41:14 -0500

    Finish export confirmation menu

--- app.go
+++ app.go
@@ -26,14 +26,14 @@ func (a *App) ReadTabletDocs(tablet_addr string) error {
 	return a.rm_reader.Read(tablet_addr)
 }
 
-/*func (a *App) GetFolderCheckboxes(id network.DocId) []bool {
-	return a.checkboxes.Get(id, &a.rm_reader)
-}*/
-
 func (a *App) GetTabletFolder(id backend.DocId) []backend.DocInfo {
 	return a.rm_reader.GetFolder(id)
 }
 
 func (a *App) IsIpValid(s string) bool {
 	return backend.IsIpValid(s)
+}
+
+func (a *App) GetElementsByIds(ids []backend.DocId) []backend.DocInfo {
+	return a.rm_reader.GetElementsByIds(ids)
 }
---
+++ frontend/src/pages/Export.svelte
@@ -0,0 +1,10 @@
+<script>
+    import { Navbar, Spinner } from "flowbite-svelte";
+
+</script>
+<div>
+    <Navbar color="blue" class="sticky top-0">
+        <h1 class="font-bold m-auto">Export</h1>
+    </Navbar>
+    <Spinner />
+</div>
